d5a66bfde7372331fd97f7325e6ab813a51db7df
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 11:37:47 -0500

    Init

--- src/main/java/com/github/javezki/App.java
+++ src/main/java/com/github/javezki/App.java
@@ -1,5 +1,7 @@
 package com.github.javezki;
 
+import net.dv8tion.jda.api.JDABuilder;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        JDABuilder builder = JDABuilder.createDefault("");
     }
 }
